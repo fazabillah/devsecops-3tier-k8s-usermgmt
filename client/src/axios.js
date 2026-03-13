81c00b3402402991b94592dac8c5674c04ea1c43
Faza Billah <fazabillah@gmail.com>
2026-03-13 15:37:51 +0100

    fix: set learndevops.my as default API base URL for production

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API || 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API || 'https://learndevops.my',
 });
 
 instance.interceptors.request.use((config) => {
